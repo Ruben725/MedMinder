553f6f1b9e12fbde11c1634281877607d27007cf
Ruben25 <ruben2xmartinez@gmail.com>
2024-11-07 00:18:45 -0800

    Firebase setup successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 //import 'package:medminder/splashscreen.dart';
 import 'package:medminder/getStarted/getStarted.dart';
 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MedMinder());
 }
 
