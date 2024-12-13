512af577ae98aa33a83c8066bdb100db7525b518
Nam Bui <namdinhbui@gmail.com>
2024-12-12 21:24:16 -0800

    added files for notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
-import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:medminder/Home/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:medminder/Home/home.dart';
+import 'package:medminder/Notification/notification.dart';
+//import 'package:medminder/Notification/notiTest.dart';
+//import 'package:medminder/Schedule/scheduleInfo.dart';
 import 'package:medminder/splashscreen.dart';
 //import 'package:medminder/getStarted/getStarted.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await NotificationRem.init();
+  await FirebaseAuth.instance.setLanguageCode('en');
+  tz.initializeTimeZones();
   runApp(const MedMinder());
 }
 
